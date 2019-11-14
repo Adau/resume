5da04a5ba2bcab666fadaf41be0f92181efb56fe
Adrien Dauvergne <adauvergne@makheia.com>
2019-11-14 17:05:29 +0100

    Initialiser la page

---
+++ src/components/Resume.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="resume">
+    Hello World!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Resume'
+}
+</script>
+
+<style scoped lang="scss">
+.resume {
+  width: 1020px;
+  margin: auto;
+  background-color: #fff;
+}
+</style>
